b30690b8765afc2a035aac65bc6ab5e7bc14e766
norinats <47509793+norinats@users.noreply.github.com>
2019-04-29 17:07:59 -0600

    update btree

--- BTree.java
+++ BTree.java
@@ -81,21 +81,96 @@ public class BTree {
 	/**
 	 * Insert a sequence into the BTree
 	 * @param sequence
+	 * @throws IOException 
 	 */
-	public void BTreeInsert(String sequence) {
+	public void BTreeInsert(String sequence) throws IOException {
 		//TODO
 		TreeObject newObject = new TreeObject(sequence, 10);
 		BTreeNode r = root;
 		if(root.getNumKeys() == maxKeys) {
+			numNodes++;
 			BTreeNode s = allocateNode();
 			root = s;
 			s.setLeaf(false);
-			s.setNumKeys(0);
-			s.setChild(1, r);
+			s.addChildPointer(r);
 			BTreeSplit(s, 1, r);
 			BTreeInsertNonfull(s, newObject);
 		} else {
 			BTreeInsertNonfull(r, newObject);
+		}
+	}
+	
+	
+	/**
+	 * Helper method for BTreeInsert that will split the specified
+	 * node.
+	 */
+	private void BTreeSplit(BTreeNode parent, int childIndex, BTreeNode child) {
+		//TODO
+		BTreeNode newNode = allocateNode();
+		newNode.setLeaf(child.isLeaf());
+		
+		//split half of child node to new node z
+		for(int i = 0; i < minKeys; i++) {
+			newNode.addTreeObject(child.removeTreeObject(0), i);
+		}
+		//if newNode is not a leaf then 
+		if(child.isLeaf() == false) {
+			for(int j = 0; j < degree; j++) {
+				newNode.addChildPointer(child.removeChildPointer(j+1));
+			}
+		}
+		
+		//move all parent's keys and children over one to allow for insertion
+		//of node being moved up from child
+		//child.setNumKeys(minKeys);
+		for(int k = parent.getNumKeys(); k >= childIndex+1; k--) {
+			parent.addChildPointer(k+1, parent.getChildPointer(k));
+		}
+		//insert child pointer of new node to parent node
+		parent.addChildPointer(childIndex+1, newNode);
+		for(int m = parent.getNumKeys(); m >= childIndex; m--) {
+			parent.addTreeObject(parent.removeTreeObject(m), m+1);
+		}
+		//insert child key being moved up to parent
+		parent.addTreeObject(child.getTreeObject(degree), childIndex);
+		
+		/*
+		 * write changes of parent, child, and newNode  nodes to .gbk file*/
+		 parent.writeNode();
+		 child.writeNode();
+		 newNode.writeNode();
+		 
+	}
+	
+	private void BTreeInsertNonfull(BTreeNode node, TreeObject object) throws IOException {
+		//TODO
+		int i = node.getNumKeys();
+		if(node.isLeaf()) {
+			while(i >= 1 && object.getKey() < node.getTreeObject(i).getKey()) {
+				node.addTreeObject(node.getTreeObject(i), i+1);
+				i--;
+			}
+			node.addTreeObject(object, i+1);
+			
+			//disk-write(node);
+			node.writeNode();
+		} else {
+			while(i>=1 && object.getKey() < node.getTreeObject(i).getKey()) {
+				i--;
+			}
+			i++;
+			
+			//read node
+			BTreeNode childNode = retrieveNode(node.getChildPointer(i));
+			//if not a leaf then recursively 
+			if(childNode.getNumKeys() == maxKeys) {
+				BTreeSplit(node, i , childNode);
+				if(object.getKey() > node.getTreeObject(i).getKey()) {
+					i++;
+					BTreeInsertNonfull(childNode, object);
+				}
+			}
 		}
 	}
 
@@ -142,78 +217,6 @@ public class BTree {
 			System.out.println("Error - traversal failed to access node.");
 		}
 	}
-
-
-	/**
-	 * Helper method for BTreeInsert that will split the specified
-	 * node.
-	 */
-	private void BTreeSplit(BTreeNode parent, int childIndex, BTreeNode child) {
-		//TODO
-		BTreeNode z = allocateNode();
-		z.setLeaf(child.isLeaf());
-		z.setNumKeys(minKeys);
-
-		//split half of child node to new node z
-		for(int i = 1; i <= minKeys; i++) {
-			z.setKey(i,child.getKey(i+1));
-		}
-		if(child.isLeaf() == false) {
-			for(int j = 1; j <= degree; j++) {
-				z.setChild(j, child.getChild(j+1));
-			}
-		}
-
-		//move all parent's keys and children over one to allow for insertion
-		//of node being moved up from child
-		child.setNumKeys(minKeys);
-		for(int k = parent.getNumKeys()+1; k >= childIndex+1; k--) {
-			parent.setChild(k+1, parent.getChild(k));
-		}
-		//insert child pointer of new node z to parent node
-		parent.setChild(childIndex+1, z);
-		for(int m = parent.getNumKeys(); m >= childIndex; m--) {
-			parent.setKey(m+1, parent.getKey(m));
-		}
-		//insert child key being moved up to parent
-		parent.setKey(childIndex, child.getKey(degree));
-		parent.setNumKeys(parent.getNumKeys()+1);
-
-		/*
-		 * disk write parent, child, and z  nodes
-		 * disk_write(parent);
-		 * disk_write(child);
-		 * disk_write(z);
-		 */
-	}
-
-	private void BTreeInsertNonfull(BTreeNode node, TreeObject object) {
-		//TODO
-		int i = node.getNumKeys();
-		if(node.isLeaf()) {
-			while(i >= 1 && object.getKey() < node.getObject(i).getKey()) {
-				node.setKey(i+1, node.getKey(i));
-				i--;
-			}
-			node.setKey(i+1, object);
-			node.setNumKeys(node.getNumKeys()+1);
-			//disk-write(node);
-		} else {
-			while(i>=1 && object.getKey() < node.getObject(i).getKey()) {
-				i--;
-			}
-			i++;
-			//disk-read(node.getChild(i));
-			if(node.getChild(i).getNumKeys() == maxKeys) {
-				BTreeSplit(node, i , node.getChild(i));
-				if(object.getKey() > node.getObject(i).getKey()) {
-					i++;
-					BTreeInsertNonfull(node.getChild(i), object)
-				}
-			}
-		}
-	}
-
 
 	private BTreeNode allocateNode() {
 		BTreeNode newNode;
